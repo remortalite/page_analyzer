b4b76a16d15244591b93e7a16d5b22071b517a6c
Rem M. <morrig407@gmail.com>
2024-06-30 00:57:05 +0700

    Add template rendering

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -6,4 +7,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return render_template("index.html")
