32603d7c3e09eb7375dfe64a4093d7e8268e4c00
Rem M. <morrig407@gmail.com>
2024-07-02 18:23:37 +0700

    Fix keepalive error with render

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -8,6 +8,19 @@ def create_connection():
 
     DATABASE_URL = os.getenv("DATABASE_URL")
 
-    conn = psycopg2.connect(DATABASE_URL)
+    keepalive_kwargs = {
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 5,
+        "keepalives_count": 5,
+    }
+
+    try:
+        conn = psycopg2.connect(DATABASE_URL, **keepalive_kwargs)
+    except psycopg2.Error  as e:
+        print('Unable to connect!\n{0}'.format(e))
+    else:
+        print('Connected!')
+
 
     return conn
